66960ef8a1ae83a05080fa51349fa65606435d2e
Uguisu32J <uguisu32j@gmail.com>
2021-11-22 20:34:18 +0900

    Update settings

--- src/main/java/com/github/uguisu32j/hitakieditor/ui/EditorFrame.java
+++ src/main/java/com/github/uguisu32j/hitakieditor/ui/EditorFrame.java
@@ -2,14 +2,16 @@ package com.github.uguisu32j.hitakieditor.ui;
 
 import javax.swing.JFrame;
 
+import com.github.uguisu32j.hitakieditor.HitakiEditor;
+
 @SuppressWarnings("serial")
 public class EditorFrame extends JFrame {
 	public EditorFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		// setBounds(Integer.parseInt(HitakiEditor.SETTINGS.getProperty("frame.editor.x")),
-		// Integer.parseInt(HitakiEditor.SETTINGS.getProperty("frame.editor.y")),
-		// Integer.parseInt(HitakiEditor.SETTINGS.getProperty("frame.editor.width")),
-		// Integer.parseInt(HitakiEditor.SETTINGS.getProperty("frame.editor.height")));
+		setBounds(Integer.parseInt(HitakiEditor.SETTINGS.getProperty("frame.editor.x")),
+				Integer.parseInt(HitakiEditor.SETTINGS.getProperty("frame.editor.y")),
+				Integer.parseInt(HitakiEditor.SETTINGS.getProperty("frame.editor.width")),
+				Integer.parseInt(HitakiEditor.SETTINGS.getProperty("frame.editor.height")));
 		setVisible(true);
 	}
 }
