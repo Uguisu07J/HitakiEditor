be9788d0af4621e0aa5170cd9501350d892c727a
Uguisu32J <uguisu32j@gmail.com>
2021-11-26 19:10:13 +0900

    Add StatusBar

--- src/main/java/com/github/uguisu32j/hitakieditor/ui/CodePane.java
+++ src/main/java/com/github/uguisu32j/hitakieditor/ui/CodePane.java
@@ -1,9 +1,21 @@
 package com.github.uguisu32j.hitakieditor.ui;
 
+import java.nio.file.Path;
+
 import javax.swing.JTextPane;
 
 @SuppressWarnings("serial")
 public class CodePane extends JTextPane {
+    private StatusBar statusBar;
+
+    public CodePane(Path path) {
+        statusBar = new StatusBar(this);
+    }
+
+    public StatusBar getStatusBar() {
+        return statusBar;
+    }
+
     public void setLine(int line) {
 
     }
