82896f00ee74799500019dd9f8af076b0fa10090
Uguisu32J <uguisu32j@gmail.com>
2021-12-04 09:07:20 +0900

    Add reopen()

--- src/main/java/com/github/uguisu32j/hitakieditor/ui/CodePane.java
+++ src/main/java/com/github/uguisu32j/hitakieditor/ui/CodePane.java
@@ -11,11 +11,13 @@ import org.slf4j.LoggerFactory;
 public class CodePane extends JTextPane {
     private static final Logger LOGGER = LoggerFactory.getLogger(CodePane.class);
     private StatusBar statusBar;
+    private Path path;
 
     public CodePane(Path path) {
         if (LOGGER.isInfoEnabled() && path != null) {
             LOGGER.info("Opening " + path);
         }
+        this.path = path;
         statusBar = new StatusBar(this);
     }
 
@@ -25,5 +27,18 @@ public class CodePane extends JTextPane {
 
     public void setLine(int line) {
 
+    }
+
+    private void open() {
+
+    }
+
+    private void save() {
+
+    }
+
+    public void reopen() {
+        save();
+        open();
     }
 }
