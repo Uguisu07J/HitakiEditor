a47209fb09ba03744ed376dd6e42337fef641ec5
Uguisu32J <uguisu32j@gmail.com>
2021-12-12 13:00:37 +0900

    Add toColorCode()

--- src/main/java/com/github/uguisu32j/hitakieditor/ui/SettingsFrame.java
+++ src/main/java/com/github/uguisu32j/hitakieditor/ui/SettingsFrame.java
@@ -1,8 +1,12 @@
 package com.github.uguisu32j.hitakieditor.ui;
 
+import java.awt.Color;
+
 import javax.swing.JFrame;
 
 @SuppressWarnings("serial")
 public class SettingsFrame extends JFrame {
-
+    private String toColorCode(Color color) {
+        return String.format("#%02X%02X%02X", color.getRed(), color.getGreen(), color.getBlue());
+    }
 }
