f3af2307174a7b7eab0249006682e844383842ed
Uguisu32J <uguisu32j@gmail.com>
2021-12-19 15:32:58 +0900

    Update Logback config

---
+++ src/main/java/com/github/uguisu32j/hitakieditor/HFileAppender.java
@@ -0,0 +1,16 @@
+package com.github.uguisu32j.hitakieditor;
+
+import java.io.File;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.FileAppender;
+import ch.qos.logback.core.rolling.TriggeringPolicy;
+
+public class HFileAppender extends FileAppender<TriggeringPolicy<ILoggingEvent>> {
+    public HFileAppender() {
+        setFile(HitakiEditor.APP_DATA + File.separator + "logs" + File.separator
+                + DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss").format(LocalDateTime.now()) + ".log");
+    }
+}
--- src/main/java/com/github/uguisu32j/hitakieditor/HitakiEditor.java
+++ src/main/java/com/github/uguisu32j/hitakieditor/HitakiEditor.java
@@ -31,7 +31,7 @@ import com.github.uguisu32j.hitakieditor.ui.EditorFrame;
 
 public class HitakiEditor {
     public static final String VERSION = "1.0.0";
-    private static final String APP_DATA = getAppData();
+    public static final String APP_DATA = getAppData();
     public static final HProperties SETTINGS = new HProperties(getDefaultSettings(),
             Path.of(APP_DATA, "settings.properties"));
     public static final HProperties WINDOW_SIZE = new HProperties(getDefaultWindowSize(),
