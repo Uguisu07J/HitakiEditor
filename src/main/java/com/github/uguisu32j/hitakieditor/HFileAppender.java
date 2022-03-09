6feedb0128dff92c6946e69b0ac42966a63291c0
Uguisu32J <uguisu32j@gmail.com>
2022-03-09 10:36:10 +0900

    Use "/" in path

--- src/main/java/com/github/uguisu32j/hitakieditor/HFileAppender.java
+++ src/main/java/com/github/uguisu32j/hitakieditor/HFileAppender.java
@@ -1,6 +1,5 @@
 package com.github.uguisu32j.hitakieditor;
 
-import java.io.File;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import ch.qos.logback.classic.spi.ILoggingEvent;
@@ -9,7 +8,7 @@ import ch.qos.logback.core.rolling.TriggeringPolicy;
 
 public class HFileAppender extends FileAppender<TriggeringPolicy<ILoggingEvent>> {
     public HFileAppender() {
-        setFile(HitakiEditor.APP_DATA_DIR + File.separator + "logs" + File.separator
+        setFile(HitakiEditor.DATA_DIR + "/logs/"
                 + DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss").format(LocalDateTime.now()) + ".log");
     }
 }
